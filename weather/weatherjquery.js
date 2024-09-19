3c89d4630d89aac3fda0bf0936ca0790229c1a3d
Lee <leo@shingu.ac.kr>
2024-09-19 11:31:18 +0900

    jquery로  변경

---
+++ weather/weatherjquery.js
@@ -0,0 +1,23 @@
+let WeatherObject = {
+	getWeather: function() {
+		$.ajax({
+			type: "GET",
+			url: 'https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35',
+		}).done(function(response) {
+            let wdata = response
+            let exdata = response.weather[0];
+        
+            temp.innerText = wdata.main.temp + "°C";
+            min.innerText = wdata.main.temp_min;
+            max.innerText = wdata.main.temp_max;
+            wind.innerText = wdata.wind.speed;
+        
+            weather.innerText = exdata.main + "," + exdata.description;
+            icon.setAttribute('src', icon_url + exdata.icon + ".png");
+		}).fail(function(error) {
+			alert("!/js/user.js에서 에러발생: " + error.statusText);
+		});
+	},
+ }
+
+ WeatherObject.getWeather();
