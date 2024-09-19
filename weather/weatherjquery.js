2440f30dfe9256099d8b870a80d01c9a7d253361
Lee <leo@shingu.ac.kr>
2024-09-19 12:15:47 +0900

    weather parsing 실습

--- weather/weatherjquery.js
+++ weather/weatherjquery.js
@@ -4,6 +4,10 @@ let WeatherObject = {
 			type: "GET",
 			url: 'https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35',
 		}).done(function(response) {
+
+            console.log(response)
+            // alert(response.weather[0].main)
+
             let wdata = response
             let exdata = response.weather[0];
         
