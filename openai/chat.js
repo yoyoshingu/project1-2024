51c5c4d0c982cad6e8906ee66c90cc81980b2e5a
Lee <leo@shingu.ac.kr>
2024-10-08 12:19:53 +0900

    chatgpt drawing 지원

--- openai/chat.js
+++ openai/chat.js
@@ -1,9 +1,14 @@
+// https://github.com/yoyoshingu/project1-2024
+
 OPENAPI_KEY = ""
 
 smodel35 = "gpt-3.5-turbo"
 smodelmini = "gpt-4o-mini"
-squestion = "황진이가 누구지"
+smodel40o = "gpt-4o"
+// squestion = "황진이가 누구지"
+
 function talk(){
+    squestion = txtMsg.value
     data ={
         model: smodelmini,
         messages: [
@@ -24,10 +29,41 @@ function talk(){
         contentType: "application/json; charset=utf-8"
     }).done( function(response){
         console.log(response)
-        alert(response.choices[0].message.content)
-    }).fail(
-
-    )
+        // alert(response.choices[0].message.content)
+        txtOut.value = response.choices[0].message.content
+    }).fail(function(error){
+        console.log(error)
+        errormsg = error.status + " : " + error.responseJSON.error.code + " - " + error.responseJSON.error.message
+        txtOut.value = errormsg
+    })
 }
 
-talk()
+
+function draw(){
+    squestion = txtMsg.value
+    data ={
+        prompt: squestion,
+        n: 2,
+        size:"512x512"
+    }
+
+    $.ajax({
+        type:"POST",
+        url: "https://api.openai.com/v1/images/generations",
+        headers:{
+            "Authorization": "Bearer " + OPENAPI_KEY
+        },
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8"
+    }).done( function(response){
+        console.log(response)
+        // alert(response.choices[0].message.content)
+        gimage.src = response.data[0].url
+        gimage2.src = response.data[1].url
+    }).fail(function(error){
+        console.log(error)
+        errormsg = error.status + " : " + error.responseJSON.error.code + " - " + error.responseJSON.error.message
+        txtOut.value = errormsg
+    })
+}
+
